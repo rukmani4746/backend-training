43f89b1448dee3d32d776916a41b0dc5ad9d1fa9
rukmani4746 <you@example.com>
2022-09-20 15:45:34 +0530

    userController and validation have completed

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,16 +1,17 @@
-const bookModel = require('../models/bookModel')
+// const bookModel = require('../models/bookModel')
 const userModel = require('../models/userModel')
-const reviewModel = require('../models/reviewModel')
+// const reviewModel = require('../models/reviewModel')
 const validator = require('../validator/validator')
 
 
 let createUserDocument = async function (req, res) {
     try {
+        
 
         let document = req.body
         let { title ,name , phone , email , password , address } = document
 
-        if (Object.keys(document).length === 0) return res.status(400).send     ({status:false ,msg:"require data to create document"})
+        if (Object.keys(document).length === 0) return res.status(400).send({status:false ,msg:"require data to create document"})
 
         if(!title) return res.status(400).send({status:false,msg:"title must be present"})
         if(!validator.isTitleValid(title)) return res.status(400).send({status: false , msg: "title is not valid"})
@@ -40,12 +41,37 @@ let createUserDocument = async function (req, res) {
        return res.status(201).send({status:true, data: savedData })
  }
     catch {
-        (error) 
-            res.status(500).send({ status: false, msg: error.message })
+        (err) 
+          return res.status(500).send({ status: false, msg: err.message })
         }
+    }
+
+
+    const loginUser=async (req, res)=>{
+        try {
+    
+        let { email, password } =req.body;
+    
+        if (!email){ return res.status(400).send({ status:false, message: ' email cant be empty' });} 
+    
+        if (!password){return res.status(400).send({ status:false, message: ' password cant be empty' });} 
+    
+        let emailpasswordcheck = await userModel.findOne({email:email,password:password})
+    
+        if(!emailpasswordcheck){return res.status(400).send({ status:false, message: ' plz provide valide email and password' });}
+        let token=jwt.sign({ userid:emailpasswordcheck._id .toString()},"GroupNo55",{expiresIn:"2hr"}) 
+        res.status(201).send({ status:true,message:"Sucessfull Login", data:{token:token}
+        
+    })}
+    catch (error) {
+        return res.status(500).send({ status:false, message: error.message });
+        
+    }
     }
 
 
 
 module.exports.createUserDocument = createUserDocument
+module.exports.loginUser = loginUser
+
 
---
+++ src/validator/validator.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+// VALIDATION FUNCTION FOR DIFFERENT ATTRIBUTES-----
+const isValid = function (value) {
+    if (typeof value === "string" && value.trim().length == 0) return false
+    return true
+}
+
+// VALIDATION FUNCTION FOR TITLE----
+const isTitleValid= function(value){
+    if (value == "Mr" ||value =="Miss" ||value =="Mrs") return true
+    return false
+}
+
+// VALIDATION FUNCTION FOR PHONE NO.-----
+const isPhoneValid = function (value){
+    if(! /^[6-9]\d{9}$/.test(value)){
+        return false
+    }
+    return true
+}
+
+// VALIDATION FUNCTION FOR EMAIL-----
+const isEmailValid = function(value){
+    const email = emailValidator.validate(value)
+    if(!email) return false
+    return true
+}
+module.exports={isValid,isTitleValid,isPhoneValid,isEmailValid}
