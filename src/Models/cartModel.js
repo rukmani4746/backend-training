5851ad0194cbf03544936da59518b6caaeba9126
salman170 <salmanansari02120@gmail.com>
2022-10-17 19:01:28 +0530

    Cart Schema updated

--- src/Models/cartModel.js
+++ src/Models/cartModel.js
@@ -7,7 +7,8 @@ const cartSchema = new mongoose.Schema(
         userId: { type: ObjectId, ref: "user", required: true, trim: true, },
         items: [{
             productId: { type: ObjectId, ref: "Product", required: true, trim: true, },
-            quantity: { type: Number, required: true, trim: true }
+            quantity: { type: Number, required: true, trim: true },
+            _id:false
         }],
         totalPrice: { type: Number, required: true, trim: true, },
         totalItems: { type: Number, required: true, trim: true, },
