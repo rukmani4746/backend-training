3cc1af3096a093f0ab09ca5d8fbc2ea56ebdfab2
rukmani4746 <you@example.com>
2023-05-02 21:48:14 +0530

    quiz can be get now

--- src/controllers/quiz.ts
+++ src/controllers/quiz.ts
@@ -1,49 +1,68 @@
-import {Request , Response, NextFunction } from 'express';
-import Quiz from '../models/quiz';
-
+import { Request, Response, NextFunction } from "express";
+import Quiz from "../models/quiz";
+import ProjectError from "../helper/error";
 interface ReturnResponse {
-    status: "success" | "error";
-    message: String;
-    data: {} | [] ;
-  }
-  
-  
+  status: "success" | "error";
+  message: String;
+  data: {} | [];
+}
 
-const createQuiz = async (req:Request, res:Response, next: NextFunction) => {
-
-try {
-    
+const createQuiz = async (req: Request, res: Response, next: NextFunction) => {
+  try {
     const created_by = req.userId;
 
     const name = req.body.name;
     const questions_list = req.body.questions_list;
     const answers = req.body.answers;
- 
-     const quiz = new Quiz({name,questions_list,answers});
-     const result = await quiz.save();
-     const resp:ReturnResponse = ({status: "success", message: "Quiz created successfully",
- data: {quizId:result._id}})
-     res.status(201).send(resp);
 
-} catch (error) {
+    const quiz = new Quiz({ name, questions_list, answers });
+    const result = await quiz.save();
+    const resp: ReturnResponse = {
+      status: "success",
+      message: "Quiz created successfully",
+      data: { quizId: result._id },
+    };
+    res.status(201).send(resp);
+  } catch (error) {
     next(error);
-}
-
-}
-
-
-const getQuiz = (req:Request, res:Response) => {
-    res.send(req.params.quizId);
-}
-const updateQuiz = (req:Request, res:Response) => {
-    res.send(req.body)
-}
-const deleteQuiz = (req:Request, res:Response) => {
-    res.send(req.params.quizId)
-}
-const publishQuiz = (req:Request, res:Response) => {
-    res.send(req.body)
-}
+  }
+};
 
 
-export { createQuiz , getQuiz , updateQuiz , deleteQuiz , publishQuiz }
+
+const getQuiz = async (req: Request, res: Response, next:NextFunction) => {
+  try {
+    const quizId = req.params.quizId;
+  const quiz = await Quiz.findById(quizId,{name:1,questions_list:1,answers:1});
+  console.log(quiz);
+  
+  if(!quiz){
+    const err = new ProjectError("Quiz not found");
+    err.statusCode = 404;
+    throw err;
+  }
+  const resp:ReturnResponse = { status:"success" ,message:"quiz",data:quiz};
+  res.status(200).send(resp)
+  
+  } catch (error) {
+    next(error)
+  }
+};
+
+
+const updateQuiz = (req: Request, res: Response) => {
+  res.send(req.body);
+};
+
+
+const deleteQuiz = (req: Request, res: Response) => {
+  res.send(req.params.quizId);
+};
+
+
+const publishQuiz = (req: Request, res: Response) => {
+  res.send(req.body);
+};
+
+
+export { createQuiz, getQuiz, updateQuiz, deleteQuiz, publishQuiz };
